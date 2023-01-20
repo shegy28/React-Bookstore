de78b2223b49fdce0d3d98c0fd5ed723c352eaca
PrantoshB <Pbiswas1007@gmail.com>
2023-01-20 16:25:36 +0530

    Create Navigation component

---
+++ src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+        <div>
+            <ul>
+                <li><Link to="/">Books</Link></li>
+                <li><Link to="/categories">Categories</Link></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navigation
