0aa81dd52f4ba89da81993aecf090ee87ea53601
PrantoshB <Pbiswas1007@gmail.com>
2023-01-20 16:26:13 +0530

    Create Books and Categories page

---
+++ src/pages/Books.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Book from '../components/Book';
+import Form from '../components/Form';
+
+const Books = () => {
+    return (
+        <>
+            <Book title="Dracula" author="Arthur Conan Doyle" />
+            <Form />
+        </>
+
+    )
+}
+
+export default Books
---
+++ src/pages/Categories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Categories = () => {
+    return (
+        <div>
+            <button>Check Status</button>
+        </div>
+    )
+}
+
+export default Categories;
