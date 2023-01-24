8cde4728595be39e259f84e69989849cf0474cdf
PrantoshB <Pbiswas1007@gmail.com>
2023-01-25 00:31:16 +0530

    Show status on categories page

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from '../components/Book';
 import Form from '../components/Form';
 
-const Books = () => (
-  <>
-    <Book title="Dracula" author="Arthur Conan Doyle" />
-    <Form />
-  </>
-);
+const Books = () => {
+  const books = useSelector((state) => state.books);
+  return (
+    <>
+
+      <ul>
+        {books.map((book) => (
+          <li key={book.id}>
+            <Book book={book} />
+          </li>
+        ))}
+
+      </ul>
+      <Form />
+    </>
+  );
+};
 
 export default Books;
--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-const Categories = () => (
-  <div>
-    <button type="button">Check Status</button>
-  </div>
-);
+const Categories = () => {
+  const dispatch = useDispatch();
+  const currentStatus = useSelector((state) => state.categories);
+
+  const showStatus = () => {
+    dispatch(checkStatus());
+  };
+
+  return (
+    <div>
+      <h4>{currentStatus}</h4>
+      <button type="button" onClick={showStatus}>Check Status</button>
+    </div>
+  );
+};
 
 export default Categories;
